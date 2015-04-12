5e067c772bbd2b05190da028546623b82b0e5dc5
materliu <materliu@gmail.com>
2015-04-12 22:15:06 +0800

    init！

---
+++ config.rb
@@ -0,0 +1,31 @@
+require 'compass/import-once/activate'
+require 'compass-normalize'
+# Require any additional compass plugins here.
+
+# set the Compass compile enviroment
+environment = :development
+
+# Set this to the root of your project when deployed:
+http_path = "http://img.mukewang.com/compass-demo"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
